091512e625b32e15fbddddca1365256b393f6da5
EggsyOnCode <farzansaqib2003@gmail.com>
2023-12-05 22:11:58 +0500

    Book List on customer homepage added

--- src/nav/StackNavigator.tsx
+++ src/nav/StackNavigator.tsx
@@ -2,18 +2,16 @@ import { createStackNavigator } from "@react-navigation/stack";
 import Login from "../screens/auth/Login";
 import Welcome from "../screens/Welcome";
 import CustomerHome from "../screens/Customer/CustomerHome";
+import { books } from "../types/const/data";
 
 const Stack = createStackNavigator();
 
-function StackNavigator({ theme }) {
+function StackNavigator({ theme }: { theme: any }) {
   return (
     <Stack.Navigator initialRouteName="CustomerHome">
-      <Stack.Screen
-        name="CustomerHome"
-        component={CustomerHome}
-        options={{ headerShown: false }}
-        initialParams={{ theme: theme }}
-      />
+      <Stack.Screen name="CustomerHome" options={{ headerShown: false }}>
+        {(props) => <CustomerHome {...props} theme={theme} books={books} />}
+      </Stack.Screen>
       <Stack.Screen
         name="Login"
         component={Login}
--- src/screens/Customer/CustomerHome.tsx
+++ src/screens/Customer/CustomerHome.tsx
@@ -1,72 +1,81 @@
-import { View, StyleSheet } from "react-native";
 import React, { useState } from "react";
-import {
-  Button,
-  Provider as PaperProvider,
-  withTheme,
-  Text,
-  Searchbar,
-} from "react-native-paper";
+import { View, StyleSheet, FlatList } from "react-native";
+import { Text, Searchbar, withTheme } from "react-native-paper";
 import { SafeAreaView } from "react-native-safe-area-context";
 import BookCard from "../../components/BookCard";
-import { book } from "../../types/const/data";
+import { Book } from "../../types/Book";
+import { ScrollView } from "react-native-gesture-handler";
 
-function CustomerHome({ theme }) {
+interface CustomerHomeProps {
+  theme: any;
+  books: Book[];
+}
+
+const CustomerHome: React.FC<CustomerHomeProps> = ({ theme, books }) => {
   const [searchQ, setSearchQ] = useState<string>("");
 
-  const styles = React.useMemo(
-    () =>
-      StyleSheet.create({
-        container: {
-          flex: 1,
-          paddingLeft: 15,
-          paddingRight: 15,
-          paddingBottom: 15,
-          justifyContent: "flex-start",
-          alignItems: "flex-start",
-          backgroundColor: theme.colors.primary,
-        },
-        title: {
-          marginBottom: 30,
-          color: theme.colors.sec2,
-        },
-        search: {
-            backgroundColor: theme.colors.green,
-            marginBottom: 20
-        },
-        cardContainer: {
-            justifyContent: "center",
-            alignItems: "center"
-        },
-        card: {
-            width: "100%",
-            backgroundColor: theme.colors.sec2,
-        }
-      }),
-    [theme]
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      paddingLeft: 15,
+      paddingRight: 15,
+      paddingBottom: 15,
+      justifyContent: "flex-start",
+      alignItems: "flex-start",
+      backgroundColor: theme.colors.primary,
+    },
+    title: {
+      marginBottom: 30,
+      color: theme.colors.sec2,
+    },
+    search: {
+      backgroundColor: theme.colors.green,
+      marginBottom: 20,
+    },
+    cardContainer: {
+      justifyContent: "center",
+      alignItems: "center",
+    },
+    contentContainer: {
+      paddingVertical: 40,
+    },
+  });
+
+  const renderBook = ({ item }: { item: Book }) => (
+    <View style={{ marginBottom: 25 }}>
+      <BookCard theme={theme} book={item} />
+    </View>
   );
 
   const onChangeSearch = (e: string) => {
     setSearchQ(e);
   };
+
   return (
     <View style={styles.container}>
-      <SafeAreaView>
+      <ScrollView contentContainerStyle={styles.contentContainer}>
         <Text variant="displayMedium" style={styles.title}>
           Browse Our Catalogues
         </Text>
-      </SafeAreaView>
-      <Searchbar
-        placeholder="Search Books"
-        onChangeText={onChangeSearch}
-        value={searchQ}
-        style={styles.search}
-      />
-      <View style={styles.cardContainer}>
-        <BookCard cardStyle={styles.card} theme={theme} book={book}/>
-      </View>
+
+        <Searchbar
+          placeholder="Search Books"
+          onChangeText={onChangeSearch}
+          value={searchQ}
+          style={styles.search}
+        />
+        <View style={styles.cardContainer}>
+          <FlatList
+            data={books}
+            renderItem={renderBook}
+            horizontal={false}
+            style={{ marginBottom: 20 }}
+            keyExtractor={(item) => item.title.toString()} // Assuming each book has an 'id' property
+          />
+        </View>
+      </ScrollView>
     </View>
   );
-}
+};
 
-export default withTheme(CustomerHome);
+export default (CustomerHome);
