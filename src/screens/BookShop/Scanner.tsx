f2d9884c0c4d8630fcd36073a48b545d55b2a4fa
EggsyOnCode <farzansaqib2003@gmail.com>
2023-12-10 00:30:57 +0500

    ocr successful using googlel cloud vision api

--- src/screens/BookShop/Scanner.tsx
+++ src/screens/BookShop/Scanner.tsx
@@ -1,5 +1,11 @@
 import React, { useState, useEffect } from "react";
-import { View, StyleSheet, Image } from "react-native";
+import {
+  View,
+  StyleSheet,
+  Image,
+  ActivityIndicator,
+  ScrollView,
+} from "react-native";
 import { Button, Avatar, withTheme, Text } from "react-native-paper";
 import * as ImagePicker from "expo-image-picker";
 import * as FileSystem from "expo-file-system";
@@ -7,6 +13,7 @@ import { Dimensions } from "react-native";
 import axios from "axios";
 import Config from "react-native-config";
 import { manipulateAsync } from "expo-image-manipulator";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const api_key = Config.OCR_API;
 
@@ -29,21 +36,22 @@ const ScannerScreen: React.FC<ScannerScreenProps> = ({ theme, navigation }) => {
     () =>
       StyleSheet.create({
         container: {
-          flex: 1,
           justifyContent: "center",
           alignItems: "center",
           backgroundColor: theme.colors.primary,
+          padding: 20,
+          paddingTop: 60,
         },
         button: {
-          marginTop: 15,
           paddingVertical: 8,
           width: "50%",
           backgroundColor: theme.colors.secondary,
           marginBottom: 12,
         },
         title: {
-          marginBottom: 30,
+          marginBottom: 20,
           color: theme.colors.sec2,
+          textAlign: "center",
         },
       }),
     [theme]
@@ -52,22 +60,14 @@ const ScannerScreen: React.FC<ScannerScreenProps> = ({ theme, navigation }) => {
   const windowWidth = Dimensions.get("window").width;
   const windowHeight = Dimensions.get("window").height;
   const [image, setImage] = useState<any>();
-  const [text, setText] = useState<string>();
-
+  const [words, setWords] = useState<string | null>(null);
+  const [proc, setProc] = useState<boolean>(false);
   // Load images on startup
   useEffect(() => {}, []);
 
   const apiCall = async (img: any) => {
+    setProc(true);
     const api_key = "AIzaSyDTpcRPc-44RydvSTDu6Oh8lrSuw2vSE_Q";
-    // const options = {
-    //   headers: {
-    //     apikey: api_key,
-    //   },
-    //   params: {
-    //     base64Image: img,
-    //     filetype: "jpg",
-    //   },
-    // };
 
     const apiUrl = `https://vision.googleapis.com/v1/images:annotate?key=${api_key}`;
     const reqData = {
@@ -82,7 +82,10 @@ const ScannerScreen: React.FC<ScannerScreenProps> = ({ theme, navigation }) => {
     };
 
     const res = await axios.post(apiUrl, reqData);
+
     console.log(res.data.responses[0].fullTextAnnotation.text);
+    setWords(res.data.responses[0].fullTextAnnotation.text);
+    setProc(false);
   };
 
   const launchCamera = async () => {
@@ -103,15 +106,17 @@ const ScannerScreen: React.FC<ScannerScreenProps> = ({ theme, navigation }) => {
 
   return (
     <View style={styles.container}>
-      <Text variant="displayMedium" style={styles.title}>
-        Add a Book
-      </Text>
-      <Avatar.Icon
-        icon="camera"
-        style={{ backgroundColor: "transparent" }}
-        size={150}
-        color={theme.colors.accent}
-      />
+      <View style={{ alignItems: "center", marginBottom: 20 }}>
+        <Text variant="displayMedium" style={styles.title}>
+          Add a Book
+        </Text>
+        <Avatar.Icon
+          icon="camera"
+          style={{ backgroundColor: "transparent", marginTop: 10 }}
+          size={150}
+          color={theme.colors.accent}
+        />
+      </View>
       <Button
         mode="contained"
         onPress={() => launchCamera()}
@@ -123,18 +128,41 @@ const ScannerScreen: React.FC<ScannerScreenProps> = ({ theme, navigation }) => {
       >
         Scan
       </Button>
-      {image && (
-        <Image
-          source={{ uri: image }}
-          style={{
-            height: windowHeight * 0.4,
-            width: windowWidth * 0.8,
-            borderWidth: 1,
-            borderRadius: 11,
-          }}
-        />
-      )}
-      {}
+      <ScrollView>
+        {image && (
+          <Image
+            source={{ uri: image }}
+            style={{
+              height: windowHeight * 0.4,
+              width: windowWidth * 0.8,
+              borderWidth: 1,
+              borderRadius: 11,
+              marginTop: 20,
+              marginBottom: 50,
+            }}
+          />
+        )}
+      </ScrollView>
+      <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
+        <View>
+          {!proc ? (
+            words && (
+              <Text style={{ color: "red", marginBottom: 6 }}>{words}</Text>
+            )
+          ) : (
+            <View
+              style={{
+                flex: 1,
+                justifyContent: "center",
+                alignItems: "center",
+                marginTop: 20,
+              }}
+            >
+              <ActivityIndicator size="large" color="blue" />
+            </View>
+          )}
+        </View>
+      </ScrollView>
     </View>
   );
 };
