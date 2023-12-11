91e35bc1a708adb4f90aead663a01f137ac4edf3
EggsyOnCode <farzansaqib2003@gmail.com>
2023-12-12 02:21:23 +0500

    added screen for biz registration: completed

--- src/screens/BookShop/RegisterBookStore.tsx
+++ src/screens/BookShop/RegisterBookStore.tsx
@@ -1,7 +1,11 @@
-import { View, StyleSheet } from "react-native";
-import React from "react";
-import { Button, Avatar, withTheme, Text } from "react-native-paper";
+import { View, StyleSheet, Dimensions } from "react-native";
+import React, { useState } from "react";
+import { Button, Avatar, withTheme, Text, TextInput } from "react-native-paper";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { ScrollView } from "react-native-gesture-handler";
 
+const windowWidth = Dimensions.get("window").width;
+const windowHeight = Dimensions.get("window").height;
 interface props {
   theme: any;
   navigation: any;
@@ -16,7 +20,8 @@ const RegisterBookStore: React.FC<props> = ({ theme, navigation }) => {
           alignItems: "flex-start",
           backgroundColor: theme.colors.primary,
           padding: 20,
-          flex: 1,
+          flexGrow: 1,
+          paddingRight: 2,
           paddingTop: 60,
         },
         button: {
@@ -25,25 +30,244 @@ const RegisterBookStore: React.FC<props> = ({ theme, navigation }) => {
           backgroundColor: theme.colors.secondary,
           marginBottom: 12,
         },
+        rampContainer: {
+          justifyContent: "center",
+          alignItems: "center",
+          backgroundColor: theme.colors.green,
+          padding: 20,
+          borderRadius: 11
+        },
         title: {
           marginBottom: 20,
           color: theme.colors.sec2,
           textAlign: "center",
         },
+        title2: {
+          marginBottom: 20,
+          color: theme.colors.sec2,
+          textAlign: "center",
+          marginTop: 30,
+        },
+        input: {
+          width: windowWidth * 0.8,
+          marginBottom: 20,
+        },
       }),
     [theme]
   );
 
+  //states
+  const [name, setName] = useState("");
+  const [shopkeeper, setShopkeeper] = useState("");
+  const [email, setEmail] = useState("");
+  const [bizHelpline, setBizHelpline] = useState("");
+  const [location, setLocation] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [bankAccountNumber, setBankAccountNumber] = useState("");
+  const [bankAccountName, setBankAccountName] = useState("");
+  const [jazzCash, setJazzCash] = useState("");
+
+  const handleNameChange = (text: string) => {
+    setName(text);
+  };
+  const handleShopkeeper = (text: string) => {
+    setShopkeeper(text);
+  };
+
+  const handleEmailChange = (text: string) => {
+    setEmail(text);
+  };
+
+  const handleBizHelplineChange = (text: string) => {
+    setBizHelpline(text);
+  };
+
+  const handleLocationChange = (text: string) => {
+    setLocation(text);
+  };
+
+  const handlePhoneNumberChange = (text: string) => {
+    setPhoneNumber(text);
+  };
+
+  const handleBankAccountNumberChange = (text: string) => {
+    setBankAccountNumber(text);
+  };
+
+  const handleBankAccountNameChange = (text: string) => {
+    setBankAccountName(text);
+  };
+
+  const handleJazzCashChange = (text: string) => {
+    setJazzCash(text);
+  };
+
+  const handleSubmit = () => {
+    // Handle form submission logic
+  };
+
   return (
     <View style={styles.container}>
-      <Text variant="displaySmall" style={styles.title}>
-        Business
-      </Text>
-      <Text variant="displaySmall" style={styles.title}>
-        OnRamping
-      </Text>
+      <ScrollView>
+        <Text variant="displaySmall" style={styles.title}>
+          Business
+        </Text>
+        <Text variant="displaySmall" style={styles.title}>
+          OnRamping
+        </Text>
 
-      
+        <Text variant="headlineMedium" style={styles.title2}>
+          Business
+        </Text>
+        <View style={styles.rampContainer} id="Business">
+          <TextInput
+            label={"Name"}
+            value={name}
+            onChangeText={handleNameChange}
+            textColor={theme.colors.black}
+            style={styles.input}
+            theme={{
+              colors: {
+                text: theme.colors.black,
+                primary: theme.colors.purple,
+                placeholder: theme.colors.black, // Add any other color properties if needed
+              },
+            }}
+          />
+
+          <TextInput
+            label={"Email"}
+            value={email}
+            onChangeText={handleEmailChange}
+            textColor={theme.colors.black}
+            style={styles.input}
+            theme={{
+              colors: {
+                text: theme.colors.black,
+                primary: theme.colors.purple,
+                placeholder: theme.colors.black, // Add any other color properties if needed
+              },
+            }}
+          />
+
+          <TextInput
+            label={"Biz Help Line"}
+            value={bizHelpline}
+            onChangeText={handleBizHelplineChange}
+            textColor={theme.colors.black}
+            style={styles.input}
+            theme={{
+              colors: {
+                text: theme.colors.black,
+                primary: theme.colors.purple,
+                placeholder: theme.colors.black, // Add any other color properties if needed
+              },
+            }}
+          />
+
+          <TextInput
+            label={"Location"}
+            value={location}
+            onChangeText={handleLocationChange}
+            textColor={theme.colors.black}
+            style={styles.input}
+            theme={{
+              colors: {
+                text: theme.colors.black,
+                primary: theme.colors.purple,
+                placeholder: theme.colors.black, // Add any other color properties if needed
+              },
+            }}
+          />
+        </View>
+
+        <Text variant="headlineMedium" style={styles.title2}>
+          Admin/Shopkeeper{"\n"}Details
+        </Text>
+
+        <View style={styles.rampContainer} id="Admin">
+          <TextInput
+            label={"Shopkeeper's Name"}
+            value={shopkeeper}
+            onChangeText={handleShopkeeper}
+            textColor={theme.colors.black}
+            style={styles.input}
+            theme={{
+              colors: {
+                text: theme.colors.black,
+                primary: theme.colors.purple,
+                placeholder: theme.colors.black, // Add any other color properties if needed
+              },
+            }}
+          />
+
+          <TextInput
+            label={"Phone Number"}
+            value={phoneNumber}
+            onChangeText={handlePhoneNumberChange}
+            textColor={theme.colors.black}
+            style={styles.input}
+            theme={{
+              colors: {
+                text: theme.colors.black,
+                primary: theme.colors.purple,
+                placeholder: theme.colors.black, // Add any other color properties if needed
+              },
+            }}
+          />
+          {/* Other Admin/Shopkeeper Details */}
+        </View>
+
+        <Text variant="headlineMedium" style={styles.title2}>
+          Banking Details
+        </Text>
+        <View style={styles.rampContainer} id="Billing">
+          <TextInput
+            label={"Bank Account Name"}
+            value={bankAccountName}
+            onChangeText={handleBankAccountNameChange}
+            textColor={theme.colors.black}
+            style={styles.input}
+            theme={{
+              colors: {
+                text: theme.colors.black,
+                primary: theme.colors.purple,
+                placeholder: theme.colors.black, // Add any other color properties if needed
+              },
+            }}
+          />
+
+          <TextInput
+            label={"Bank Account Number"}
+            value={bankAccountNumber}
+            onChangeText={handleBankAccountNumberChange}
+            textColor={theme.colors.black}
+            style={styles.input}
+            theme={{
+              colors: {
+                text: theme.colors.black,
+                primary: theme.colors.purple,
+                placeholder: theme.colors.black, // Add any other color properties if needed
+              },
+            }}
+          />
+
+          <TextInput
+            label={"Jazzcash Number"}
+            value={jazzCash}
+            onChangeText={handleJazzCashChange}
+            textColor={theme.colors.black}
+            style={styles.input}
+            theme={{
+              colors: {
+                text: theme.colors.black,
+                primary: theme.colors.purple,
+                placeholder: theme.colors.black, // Add any other color properties if needed
+              },
+            }}
+          />
+        </View>
+      </ScrollView>
     </View>
   );
 };
